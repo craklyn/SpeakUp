4055b63f43fcb077c5fb5c96f20e38296da7fbf3
Daniel Blackburn <craklyn@gmail.com>
2016-11-06 11:44:37 -0800

    Updated script

--- LadyProblems/makeSound.sh
+++ LadyProblems/makeSound.sh
@@ -1,6 +1,8 @@
 # To run:
 # source makeSound.sh  > output.out 2>&1
 
+THRESHOLD_VALUE=0.2
+
 for i in `seq 1000000`; do
   sox -d processing.wav trim 0 0.25
   sox processing.wav -b 16 processing16bit.wav
@@ -9,5 +11,8 @@ for i in `seq 1000000`; do
   rm processing16bit.wav
   cat temp.processing.out | datamash sstdev 1 > data/volumeResults.txt
   rm temp.processing.out
+
+  (wc -l < ../wordsSaid.txt | awk '{$1=$1;print}') > data/wordCount.txt
+  (wc -l < ../micVolume.txt | awk '{$1=$1;print}') > data/micTime.txt  
 done
 
