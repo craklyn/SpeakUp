4b02cf59d51fcc44fdcf265fdaeae6e682930de1
Daniel Blackburn <craklyn@gmail.com>
2016-11-05 18:16:48 -0700

    Fixing code

--- SpeechToText/writeSound.py
+++ SpeechToText/writeSound.py
@@ -1,10 +1,12 @@
 import wave
+import struct
 
 waveFile = wave.open('output16bit.wav', 'r')
 length = waveFile.getnframes()
 for i in range(0,length):
   waveData = waveFile.readframes(1)
-  data = map(ord, list(waveData))
+  data = struct.unpack("<hh", waveData)
   print(int(data[0]))
+  print(int(data[1]))
 
 
